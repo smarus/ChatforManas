1220a5d88fb274789c61835e5d3712819fd84b4a
nurik1kg <nurik1kg@gmail.com>
2016-05-02 10:39:40 +0600

    getUsername

--- app/src/main/java/com/example/ruslan/manaschatter/adt/ChatMessage.java
+++ app/src/main/java/com/example/ruslan/manaschatter/adt/ChatMessage.java
@@ -11,5 +11,9 @@ public class ChatMessage {
         this.message   = message;
         this.timeStamp = timeStamp;
     }
+    
+    public String getUsername() {
+        return username;
+    }
 
 }
