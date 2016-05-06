0b2d84585f3699a53d1014f2d2dc91ed2bd7ce32
NursRahman <nursultanrahman@gmail.com>
2016-05-06 09:07:07 +0600

    second

--- app/src/main/java/com/example/ruslan/manaschatter/callbacks/BasicCallback.java
+++ app/src/main/java/com/example/ruslan/manaschatter/callbacks/BasicCallback.java
@@ -11,18 +11,5 @@ public class BasicCallback extends Callback {
 
     }
 
-    @Override
-    public void successCallback(String channel, Object response) {
-        Log.d("PUBNUB", "Success: " + response.toString());
-    }
 
-    @Override
-    public void connectCallback(String channel, Object message) {
-        Log.d("PUBNUB", "Connect: " + message.toString());
-    }
-
-    @Override
-    public void errorCallback(String channel, PubnubError error) {
-        Log.d("PUBNUB", "Error: " + error.toString());
-    }
 }
